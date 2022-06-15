29f8d01a3ee724cb6f5ee2fd3f5591db83ce9e83
Mikkel Wessel Nielsen <65943106+miwnKEA@users.noreply.github.com>
2022-06-15 13:06:01 +0200

    Create app.py

---
+++ 4-filer/flask-file-upload/app.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
+
+UPLOADS_PATH = Path.cwd() / "static" / "uploads"
+
+app = Flask(__name__)
+
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = UPLOADS_PATH
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run()
